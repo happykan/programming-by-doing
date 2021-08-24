020bc211ceed20e45541d1b6e501726880599d0c
happykan <89062194+happykan@users.noreply.github.com>
2021-08-24 12:41:05 +0800

    letter to myself

--- Basic and Printing/A Letter to Yourself/LetterToYourself.java
+++ Basic and Printing/A Letter to Yourself/LetterToYourself.java
@@ -1,11 +1,12 @@
 public class LetterToYourself
 {
-    public static void main( Strings[] args)
+    public static void main ( String[] args )
     {
         System.out.println("+------------------------------------------+");
         System.out.println("|                                     #### |");
         System.out.println("|                                     #### |");
         System.out.println("|                                     #### |");
+        System.out.println("|                                          |");
         System.out.println("|                                          |");
         System.out.println("|                                          |");
         System.out.println("|                       Happy Kan          |");
