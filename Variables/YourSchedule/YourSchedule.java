4f406d513b8164b7baea802efebd647e8af627d3
happykan <89062194+happykan@users.noreply.github.com>
2021-08-24 17:37:28 +0800

    my class

---
+++ Variables/YourSchedule/YourSchedule.java
@@ -0,0 +1,36 @@
+public class YourSchedule
+{
+    public static void main( String[] args )
+    {
+        String course1, course2, course3, course4, course5, course6, course7, course8, teacher1, teacher2, teacher3, teacher4, teacher5, teacher6, teacher7, teacher8;
+
+        course1 = "Honors_Pre_Calculus";
+        course2 = "Honors_English";
+        course3 = "Biology_With_Lab";
+        course4 = "Honors_US_History";
+        course5 = "Economics";
+        course6 = "Honors_Expository_Writing_II";
+        course7 = "AP_Human_Geography";
+        course8 = "Java_Programming";
+
+        teacher1 = "Ms._Elaine";
+        teacher2 = "Mr._Ciancio";
+        teacher3 = "Ms._Sandra";
+        teacher4 = "Mr._Cook";
+        teacher5 = "Mr._Vincent";
+        teacher6 = "Mr._Ciancio";
+        teacher7 = "Mr._Levin";
+        teacher8 = "Ms._Tina";
+
+        System.out.println( "+-------------------------------------------------------------------+" );
+        System.out.println( "| 1 | " + course1 +"                | " + teacher1 + "               | " );
+        System.out.println( "| 2 | " + course2 + "                     | " + teacher2 + "              | " );
+        System.out.println( "| 3 | " + course3 + "                   | " + teacher3 + "               | " );
+        System.out.println( "| 4 | " + course4 + "                  | " + teacher4 + "                 | " );
+        System.out.println( "| 5 | " + course5 + "                          | " + teacher5 + "              | " );
+        System.out.println( "| 6 | " + course6 + "       | " + teacher6 + "              | " );
+        System.out.println( "| 7 | " + course7 + "                 | " + teacher7 + "                | " );
+        System.out.println( "| 8 | " + course8 + "                   | " + teacher8 + "                 | " );
+        System.out.println( "+-------------------------------------------------------------------+" );
+    }
+}
