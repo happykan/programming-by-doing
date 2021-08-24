fbf9ba80d8ffe7ea9d4084513c69cbff44edfbe0
happykan <89062194+happykan@users.noreply.github.com>
2021-08-24 14:50:46 +0800

    my turn

---
+++ Variables/UsingVariables/UsingVariables.java
@@ -0,0 +1,17 @@
+public class UsingVariables
+{
+    public static void main( String[] args )
+    {
+        String What_I_am_learning;
+        int The_room;
+        double Where_e_is_close_to;
+
+        What_I_am_learning = "Computer Science";
+        The_room = 113;
+        Where_e_is_close_to = 2.71828;
+
+        System.out.println( "This is room # " + The_room );
+        System.out.println( "e is close to " + Where_e_is_close_to );
+        System.out.println( "I am learning a bit about " + What_I_am_learning );
+    }
+}
