e6161bbecb0722870d35f2277cb4cdb35a8bc1b6
happykan <89062194+happykan@users.noreply.github.com>
2021-08-24 14:57:54 +0800

    practice

---
+++ Variables/StillUsingVariables/StillUsingVariables.java
@@ -0,0 +1,13 @@
+public class StillUsingVariables
+{
+    public static void main( String[] args )
+    {
+        String What_is_my_name;
+        int The_year_I_graduate;
+
+        What_is_my_name = "Happy Kan";
+        The_year_I_graduate = 2024;
+
+        System.out.println( "My name is " + What_is_my_name + " and I'll graduate in " + The_year_I_graduate + "," );
+    }
+}
