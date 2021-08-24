9a327f6b2ed652e673d0643764306e4f5795c047
happykan <89062194+happykan@users.noreply.github.com>
2021-08-24 22:34:49 +0800

    variables

--- Variables/MoreVariablesAndPrinting/MoreVariablesAndPrinting.java
+++ Variables/MoreVariablesAndPrinting/MoreVariablesAndPrinting.java
@@ -1,25 +1,29 @@
+//Variables More Variables and Printing 7 pts Happy Kan
 public class MoreVariablesAndPrinting
 {
     public static void main( String[] args )
     {
-        String myName, myEyes, myTeeth, myHair;
-        int myAge, myHeight, myWeight;
+        String Name, Eyes, Teeth, Hair;
+        int Age, Height, Weight;
+        double myHeight, myWeight;
 
-        myName = "Zed A. Shaw";
-        myAge = 35;
-        myHeight = 74;
-        myWeight = 180;
-        myEyes = "Blue";
-        myTeeth = "White";
-        myHair = "Brown";
+        Name = "Zed A. Shaw";
+        Age = 35;
+        Height = 74;
+        Weight = 180;
+        Eyes = "Blue";
+        Teeth = "White";
+        Hair = "Brown";
+        myHeight = 0.4535 * Weight;
+        myWeight = 2.45 * Height;
 
-        System.out.println( "Let's talk about " + myName + "." );
-        System.out.println( "He's " + myHeight + " inches tall." );
-        System.out.println( "He's " + myWeight + " pounds heavy." );
+        System.out.println( "Let's talk about " + Name + "." );
+        System.out.println( "He's " + Height + " inches (or " + myHeight + "cm) tall." );
+        System.out.println( "He's " + Weight + " pounds (or " + myWeight + "kg) heavy." );
         System.out.println( "Actually, that's not too heavy." );
-        System.out.println( "He's got " + myEyes + " eyes and " + myHair + "hair." );
-        System.out.println( "His teeth are usually " + myTeeth + " depending on the coffee" );
+        System.out.println( "He's got " + Eyes + " eyes and " + Hair + "hair." );
+        System.out.println( "His teeth are usually " + Teeth + " depending on the coffee" );
 
-        System.out.println( "If I add " + myAge + ", " + myHeight + ", and " + myWeight + " I get " + (myAge + myHeight + myWeight) + "." );
+        System.out.println( "If I add " + Age + ", " + Height + ", and " + Weight + " I get " + (Age + Height + Weight) + "." );
     }
 }
