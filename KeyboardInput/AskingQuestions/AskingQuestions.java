214f08b1763651fdd761656a47158ec58d6c1ca6
happykan <89062194+happykan@users.noreply.github.com>
2021-08-30 22:27:14 +0800

    Qs

--- KeyboardInput/AskingQuestions/AskingQuestions.java
+++ KeyboardInput/AskingQuestions/AskingQuestions.java
@@ -8,18 +8,21 @@ public class AskingQuestions
             Scanner keyboard = new Scanner(System.in);
 
             int age;
-            String height;
+            String number_of_feet, number_of_inches;
             double weight;
 
             System.out.print( "How old are you? " );
             age = keyboard.nextInt();
 
-            System.out.print( "How tall are you? " );
-            height = keyboard.next();
+            System.out.print( "How many feet tall are you? " );
+            number_of_feet = keyboard.next();
+
+            System.out.print( "And how mahy inches? " );
+            number_of_inches = keyboard.next();
 
             System.out.print( "How much do you weight? " );
             weight = keyboard.nextDouble();
 
-            System.out.println( "So you're " + age + " years old, " + height + " tall and " + weight + " heavy. " );
+            System.out.println( "So you're " + age + " years old, " + number_of_feet + "'" + number_of_inches + "'" + " tall and " + weight + " heavy." );
     }
 }
