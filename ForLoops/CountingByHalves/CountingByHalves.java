588911bae178663fa89d5e4ca86a7c88bede07b1
happykan <89062194+happykan@users.noreply.github.com>
2021-09-29 12:11:04 +0800

    1/2

---
+++ ForLoops/CountingByHalves/CountingByHalves.java
@@ -0,0 +1,13 @@
+//For Loops Counting By Halves 20 pts Happy Kan
+public class CountingByHalves
+{
+    public static void main(String[]args)
+    {
+        System.out.println("x");
+        System.out.println("------");
+        for (double x=-10; x<=10; x=x+0.5)
+        {
+            System.out.println(x);
+        }
+    }
+}
