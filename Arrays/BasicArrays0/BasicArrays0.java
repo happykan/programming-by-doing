5bc6f37b17187e1e4653f9c0dd69689e2549dfbe
happykan <89062194+happykan@users.noreply.github.com>
2021-11-12 14:14:50 +0800

    g

---
+++ Arrays/BasicArrays0/BasicArrays0.java
@@ -0,0 +1,19 @@
+//Arrays Basic Arrays 0 20 pts Happy Kan
+import java.util.Arrays;
+public class BasicArrays0
+{
+    public static void main (String[]args)
+    {
+        int a[] = {-113,-113,-113,-113,-113,-113,-113,-113,-113,-113};
+        System.out.println("slot 0 contain a " + a[0]);
+        System.out.println("slot 1 contain a " + a[1]);
+        System.out.println("slot 2 contain a " + a[2]);
+        System.out.println("slot 3 contain a " + a[3]);
+        System.out.println("slot 4 contain a " + a[4]);
+        System.out.println("slot 5 contain a " + a[5]);
+        System.out.println("slot 6 contain a " + a[6]);
+        System.out.println("slot 7 contain a " + a[7]);
+        System.out.println("slot 8 contain a " + a[8]);
+        System.out.println("slot 9 contain a " + a[9]);
+    }
+}
