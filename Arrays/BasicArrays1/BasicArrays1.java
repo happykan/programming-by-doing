edd09648ed8b8a75addad555474497045c434192
happykan <89062194+happykan@users.noreply.github.com>
2021-11-12 14:24:00 +0800

    d

---
+++ Arrays/BasicArrays1/BasicArrays1.java
@@ -0,0 +1,13 @@
+//Arrays Basic Arrays 1 30 pts Happy Kan
+import java.util.Arrays;
+public class BasicArrays1
+{
+    public static void main(String[]args)
+    {
+        int a[] = new int[10];
+        for (int i=0; i<a.length; i++)
+        {
+            System.out.println("slot " + i + " contains a -113");
+        }
+    }
+}
