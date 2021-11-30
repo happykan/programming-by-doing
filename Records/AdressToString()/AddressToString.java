e930a54bafbb5776dfbd6b7348a40ad3df005074
happykan <89062194+happykan@users.noreply.github.com>
2021-11-30 14:37:53 +0800

    r

---
+++ Records/AdressToString()/AddressToString.java
@@ -0,0 +1,46 @@
+//Records Address toString() 20 pts Happy Kan
+import java.net.URL;
+import java.util.Scanner;
+
+class Address
+{
+	String street;
+	String city;
+	String state;
+	int zip;
+
+	public String toString()
+	{
+		return (this.street + ", " + this.city + "  " + this.state + " " + this.zip);
+	}
+}
+
+public class AddressToString
+{
+	public static void main(String[] args) throws Exception
+	{
+		URL addys = new URL("https://programmingbydoing.com/a/examples/fake-addresses.txt");
+
+		Address[] addybook = new Address[5];
+
+		Scanner fin = new Scanner( addys.openStream() );
+
+		for ( int i=0; i<5; i++ )
+		{
+			addybook[i] = new Address();
+			addybook[i].street = fin.nextLine();
+			addybook[i].city   = fin.nextLine();
+			addybook[i].state  = fin.next();
+			addybook[i].zip    = fin.nextInt();
+			fin.skip("\n");
+		}
+		fin.close();
+		
+		for ( int i=0; i<5; i++ )
+		{
+			System.out.println( (i+1) + ". " + addybook[i] );
+		}
+	}
+}
+//1. Because String and Int is different data type, the result will be different.
+//2. 
