e3a3f48b364e32c8de61a7be0e049df37c94a10d
happykan <89062194+happykan@users.noreply.github.com>
2021-12-01 11:55:33 +0800

    h

---
+++ Records/BasicRecords/BasicRecords.java
@@ -0,0 +1,24 @@
+//Records Basic Records 80 pts Happy Kan
+import java.util.Scanner;
+
+class Student
+{
+    String name;
+    int grade;
+    double avg;
+}
+public class BasicRecords
+{
+    public static void main (String[]args)
+    {
+        Scanner fin = new Scanner (System.in);
+
+        Student oneS = new Student ();
+        Student second = new Student ();
+        Student third = new Student ();       
+
+        //System.
+        one.name = fin.next();
+
+    }
+}
