f9b8caf5ec58c0a6c81251e2717e0c75a639fb43
happykan <89062194+happykan@users.noreply.github.com>
2021-12-07 14:18:13 +0800

    s

---
+++ ArrayLists/BasicArrayLists2/BasicArrayLists2.java
@@ -0,0 +1,23 @@
+//ArrayLists Basic ArrayLists 2 40 pts Happy Kan
+import java.util.*;
+@SuppressWarnings("unchecked")
+public class BasicArrayLists2
+{
+    public static void main (String[]args)
+    {
+        Random r = new Random();
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        for (int i=0; i<10; i++)
+        {
+            list.add(getNumbers());
+        }
+        System.out.print("ArrayList: " + list);
+    }
+    public static int getNumbers()
+    {
+        Random r = new Random();
+        int x = 1+r.nextInt(100);
+        return x;
+    }
+}
