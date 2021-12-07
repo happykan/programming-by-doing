140ffc26643aeaf77168692a3a5bbf8affb36d40
happykan <89062194+happykan@users.noreply.github.com>
2021-12-07 13:45:08 +0800

    s

--- Records/BasicRecords/BasicRecords.java
+++ Records/BasicRecords/BasicRecords.java
@@ -6,6 +6,20 @@ class Student
     String name;
     int grade;
     double avg;
+
+    void insert(String namee, int gradee, double avgg)
+    {
+        name = namee;
+        grade = gradee;
+        avg = avgg;
+    }
+
+    void print()
+    {
+        System.out.println(name);
+        System.out.println(grade);
+        System.out.println(avg);
+    }
 }
 public class BasicRecords
 {
@@ -13,12 +27,47 @@ public class BasicRecords
     {
         Scanner fin = new Scanner (System.in);
 
-        Student oneS = new Student ();
-        Student second = new Student ();
-        Student third = new Student ();       
+        String test = fin.nextLine();
+        int test2 = fin.nextInt();
+        double test3 = fin.nextDouble();
+        String test4 = fin.nextLine();
+
+        Student oneS = new Student();
+        
+        oneS.insert(test,test2,test3);
+
+        test = fin.nextLine();
+        test2 = fin.nextInt();
+        test3 = fin.nextDouble();
+        test4 = fin.nextLine(); 
+
+        Student second = new Student();
+
+        second.insert(test,test2,test3);
+
+        test = fin.nextLine();
+        test2 = fin.nextInt();
+        test3 = fin.nextDouble();
+        test4 = fin.nextLine();
+
+        Student third = new Student();    
+
+        third.insert(test,test2,test3);
+
+        test = fin.nextLine();
+        test2 = fin.nextInt();
+        test3 = fin.nextDouble();
+        test4 = fin.nextLine();
 
         //System.
-        one.name = fin.next();
+        // oneS.name = fin.next();
+        // oneS.grade = fin.nextInt();
+        // oneS.avg = fin.nextDouble();
 
+        oneS.print();
+        second.print();
+        third.print();
+
+        System.out.print("The names are: " + test);
     }
 }
