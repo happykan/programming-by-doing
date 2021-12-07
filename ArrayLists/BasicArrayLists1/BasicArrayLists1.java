921d8d8bcec01c99d4b914fd445713c7e0d38ed3
happykan <89062194+happykan@users.noreply.github.com>
2021-12-07 13:59:09 +0800

    e

---
+++ ArrayLists/BasicArrayLists1/BasicArrayLists1.java
@@ -0,0 +1,18 @@
+//ArrayLists Basic ArrayLists 1 30 pts Happy Kan
+import java.util.*;
+@SuppressWarnings("unchecked")
+public class BasicArrayLists1
+{
+    public static void main (String[]args)
+    {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i=0; i<10; i++)
+        {
+            list.add(-113);
+        }
+        for (int i=0; i<list.size(); i++)
+        {
+            System.out.println("Slot" + i + " contains a " + list.get(i));
+        }
+    }
+}
