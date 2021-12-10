e9600bc065a3c8edd3489a2fd840c1fbf773f9a0
happykan <89062194+happykan@users.noreply.github.com>
2021-12-10 13:58:40 +0800

    d

---
+++ ArrayLists/ArrayList-ThereOrNot/ArrayListThereOrNot.java
@@ -0,0 +1,30 @@
+//ArrayLists ArrayList - There or Not? 85 pts Happy Kan
+import java.util.*;
+public class ArrayListThereOrNot
+{
+    public static void main (String[]args)
+    {
+        Random r = new Random();
+        Scanner input = new Scanner (System.in);
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i=0; i<10; i++)
+        {
+            list.add(1+r.nextInt(51));
+        }
+        System.out.print("ArrayList: " + list);
+        System.out.println();
+        System.out.print("Value to find: ");
+        int x = input.nextInt();
+        for (Integer i: list)
+        {
+            if (x==i)
+            {
+                System.out.println(x + " is in the ArrayList.");
+            }
+            else if (x!=i)
+            {
+                System.out.println(x + " is not in the ArrayList.");
+            }
+        }
+    }
+}
